1aefa993316262995dcb4a7636becb28e44591cb
Aleksei Filatov <alekfil.ds@gmail.com>
2024-07-18 20:16:35 +0600

    add .env

--- streamlit_app/app.py
+++ streamlit_app/app.py
@@ -1,27 +1,32 @@
+import os
+
 import requests
 import streamlit as st
+from dotenv import load_dotenv
+
+load_dotenv()
+api_url = os.getenv("API_URL")
 
 st.set_page_config(layout="wide")
 
-st.title(
-    "Текущие результаты разработки модели для распознавания наименований спортивных школ"
-)
+st.title("Имплементация сервиса распознавания наименований школ")
 
 school_name = st.text_input("Введите название школы")
-region = st.text_input("Введите регион школы")
+region = st.text_input("Введите регион школы (не обязательно)")
+print(api_url)
 
 if st.button("Распознать наименование"):
-    if school_name and region:
+    if school_name:
         response = requests.post(
-            "http://45.67.59.211:5001/find_matches/",
+            api_url + "find_matches/",
             json={"school_name": school_name, "region": region},
         )
         if response.status_code == 200:
             matches = response.json()
-            st.subheader("Предсказание")
+            st.subheader("Результаты распознавания")
             st.write(matches)
         else:
             st.write(response.status_code)
-            st.write("Совпадения не найдены!")
+            st.write("Ошибка работы сервиса... Сообщите разработчику")
     else:
         st.write("Пожалуйста, введите название и регион школы")
